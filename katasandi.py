da5d66c23320c3586cd9639ea8e2daf9b3e2a7cb
Zlvadityaaa <andhitya009>
2025-03-24 13:19:29 +0700

    tugas sesi 6

---
+++ katasandi.py
@@ -0,0 +1,10 @@
+password="akuganteng"
+username="zlvadityaa"
+while True:
+    username = input("masukan username anda: ")
+    password_input=input("masukan password anda: ")
+    if password_input == password :
+        print("anjayy berhasil masuk os mantap")
+        break
+    else:
+        print("maaf anda tidak bisa masuk euy,kata sandinya salah")
